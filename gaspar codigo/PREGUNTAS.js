e908d3dbcf68d0758df7a9ee2816cd4fc1bb6891
threshhg <gaspar.frias@hotmail.com>
2021-04-21 21:26:40 -0300

    cambiar pregunta

--- gaspar codigo/PREGUNTAS.js
+++ gaspar codigo/PREGUNTAS.js
@@ -1,9 +1,9 @@
 data = `[
 {"pregunta":"Cual de estos deportes dura 80 minutos?","respuesta1":"RUGBY","respuesta2":"FUTBOL","respuesta3":"HANDBALL","respuesta4":"BASQUET","correcta":"1"},
-{"pregunta":"En qué país se celebraron los primeros Juegos Olímpicos?","respuesta1":"GRECIA","respuesta2":"ESTOCOLMO","respuesta3":"LONDRES","respuesta4":"ROMA"ET","correcta":"1"},
+{"pregunta":"En qué país se celebraron los primeros Juegos Olímpicos?","respuesta1":"GRECIA","respuesta2":"ESTOCOLMO","respuesta3":"LONDRES","respuesta4":"ROMA","correcta":"1"},
 {"pregunta":"Cual de estos deportes tiene equipos de nueve jugadores","respuesta1":"FUTBOL","respuesta2":"BEISBOL","respuesta3":"CRIQUET","respuesta4":"PADEL","correcta":"2"},
 {"pregunta":"Cuál es el único país que ha jugado en cada Copa del Mundo de fútbol?","respuesta1":"FRANCIA","respuesta2":"EEUU","respuesta3":"BRASIL","respuesta4":"ARGENTINA","correcta":"3"},
-{"pregunta":"Cada cuántos años se celebran los Juegos Olímpicos?","respuesta1":"2","respuesta2":"4*","respuesta3":"6","respuesta4":"1","correcta":"2"},
+{"pregunta":"Cada cuántos años se celebran los Juegos Olímpicos?","respuesta1":"2","respuesta2":"4","respuesta3":"6","respuesta4":"1","correcta":"2"},
 {"pregunta":"A que famosa saga de ficcion pertenece el deporte Quidditch?","respuesta1":"LORD OF THE RING","respuesta2":"CANCION DE HIELO Y FUEGO","respuesta3":"EL CICLO DE TERRAMAR","respuesta4":"HARRY POTTER","correcta":"4"},
 {"pregunta":"Cuántos anillos tiene la bandera olímpica?","respuesta1":"3","respuesta2":"5","respuesta3":"2","respuesta4":"6","correcta":"2"},
 {"pregunta":"Para qué elemento »Hg« es el símbolo químico?","respuesta1":"POTASIO","respuesta2":"MERCURIO","respuesta3":"ORO","respuesta4":"HIDROGENO","correcta":"2"},
