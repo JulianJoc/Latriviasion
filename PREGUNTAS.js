bae7f8568f3efa6cce1bda95f3c88c65ee68b999
JulianJoc <julianjoc2014@gmail.com>
2021-04-22 18:00:16 -0300

    elpush

---
+++ PREGUNTAS.js
@@ -0,0 +1,12 @@
+data = `[
+{"pregunta":"Cual de estos deportes dura 80 minutos?","respuesta1":"RUGBY","respuesta2":"FUTBOL","respuesta3":"HANDBALL","respuesta4":"BASQUET","correcta":"1"},
+{"pregunta":"En qué país se celebraron los primeros Juegos Olímpicos?","respuesta1":"GRECIA","respuesta2":"ESTOCOLMO","respuesta3":"LONDRES","respuesta4":"ROMA","correcta":"1"},
+{"pregunta":"Cual de estos deportes tiene equipos de nueve jugadores","respuesta1":"FUTBOL","respuesta2":"BEISBOL","respuesta3":"CRIQUET","respuesta4":"PADEL","correcta":"2"},
+{"pregunta":"Cuál es el único país que ha jugado en cada Copa del Mundo de fútbol?","respuesta1":"FRANCIA","respuesta2":"EEUU","respuesta3":"BRASIL","respuesta4":"ARGENTINA","correcta":"3"},
+{"pregunta":"Cada cuántos años se celebran los Juegos Olímpicos?","respuesta1":"2","respuesta2":"4","respuesta3":"6","respuesta4":"1","correcta":"2"},
+{"pregunta":"A que famosa saga de ficcion pertenece el deporte Quidditch?","respuesta1":"LORD OF THE RING","respuesta2":"CANCION DE HIELO Y FUEGO","respuesta3":"EL CICLO DE TERRAMAR","respuesta4":"HARRY POTTER","correcta":"4"},
+{"pregunta":"Cuántos anillos tiene la bandera olímpica?","respuesta1":"3","respuesta2":"5","respuesta3":"2","respuesta4":"6","correcta":"2"},
+{"pregunta":"Para qué elemento »Hg« es el símbolo químico?","respuesta1":"POTASIO","respuesta2":"MERCURIO","respuesta3":"ORO","respuesta4":"HIDROGENO","correcta":"2"},
+{"pregunta":"Cuál es el planeta más caliente del sistema solar?","respuesta1":"VENUS","respuesta2":"MERCURIO","respuesta3":"JUPITER","respuesta4":"MARTE","correcta":"1"},
+{"pregunta":"Qué desastre natural se mide con una escala de Richter?","respuesta1":"TORNADOS","respuesta2":"TSUNAMIS","respuesta3":"TERREMOTOS","respuesta4":"ERUPCIONES","correcta":"3"}
+]`;
